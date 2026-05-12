44069581e17242ac9a2f017fa8dfd990d2bf0788
TheBlunatic <theblunatic@outlook.com>
2026-05-12 22:58:51 +0100

    Config, icon, and prep for args input

---
+++ TheBlunatic's Lore Printer 9000/Config.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoreApp
+{
+    public interface IConfigParameter
+    {
+        string ID { get; }
+        void ReturnToDefault();
+        bool TrySetValue(string value);
+        string GetFileRepresentation();
+        ConsoleColor GetMostSuitableValueColor();
+        Type GetPropertyType();
+    }
+    public class ConfigParameter<T> : IConfigParameter
+    {
+        public string ID { get; }
+        public T Value { get; set; }
+        public T DefaultValue { get; }
+
+        public ConfigParameter(string id, T defaultValue)
+        {
+            ID = id;
+            Value = defaultValue;
+            DefaultValue = defaultValue;
+        }
+
+        public static implicit operator T(ConfigParameter<T> input)
+        {
+            return input.Value;
+        }
+
+        public void ReturnToDefault() => Value = DefaultValue;
+
+        public Type GetPropertyType() => typeof(T);
+        public bool TrySetValue(string value)
+        {
+            try
+            {
+                object newVal = Convert.ChangeType(value, typeof(T));
+                Value = (T)newVal;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public string GetFileRepresentation() => $"{ID} = {Value}";
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+        public ConsoleColor GetMostSuitableValueColor()
+        {
+            if (typeof(T) == typeof(bool))
+            {
+                if (Value.ToString() == "false") return ConsoleColor.Red;
+                return ConsoleColor.Green;
+            }
+            if (typeof(T) == typeof(string))
+            {
+                return ConsoleColor.White;
+            }
+            return ConsoleColor.Gray;
+        }
+    }
+
+    public class Config
+    {
+        const string COMMENT_IDENTIFIER = "#";
+
+        public Dictionary<string, IConfigParameter> ParameterDictionary;
+
+        public ConfigParameter<string> InputFolder { get; set; } = new ConfigParameter<string>(InputFolderID, "put images here");
+        const string InputFolderID = "Input Folder";
+        public ConfigParameter<string> OutputFolder { get; set; } = new ConfigParameter<string>(OutputFolderID, "output will be sent here");
+        const string OutputFolderID = "Output Folder";
+        public ConfigParameter<bool> UseInstallLocationAsActiveDirectory { get; set; } = new ConfigParameter<bool>(UseInstallLocationAsActiveDirectoryID, true);
+        const string UseInstallLocationAsActiveDirectoryID = "Use Install Location As Active Directory";
+
+        public Config()
+        {
+            ParameterDictionary = new Dictionary<string, IConfigParameter>();
+
+            void add(IConfigParameter configParameter)
+            {
+                ParameterDictionary.Add(configParameter.ID, configParameter);
+            }
+
+            add(InputFolder);
+            add(OutputFolder);
+            add(UseInstallLocationAsActiveDirectory);
+        }
+
+        static (string, string) FileToProperty(string line) => (line.Substring(0, line.IndexOf('=') - 1), line.Substring(line.IndexOf('=') + 2));
+
+        public Config(string[] configFile) : this()
+        {
+            int expectedSuccesses = ParameterDictionary.Count;
+
+            HashSet<string> successes = new HashSet<string>();
+
+            foreach (string line in configFile)
+            {
+                if (line.StartsWith(COMMENT_IDENTIFIER)) continue;
+                if (line.Trim().Length == 0) continue;
+
+                string id, value;
+                (id, value) = FileToProperty(line);
+
+                successes.Add(id);
+
+                if (ParameterDictionary.TryGetValue(id, out IConfigParameter configParameter))
+                {
+                    if (!configParameter.TrySetValue(value))
+                    {
+                        throw new ArgumentException($"Config failed to load parameter '{id}' to value '{value}'");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Config file contained an unrecognised parameter id.");
+                }
+            }
+
+            if (expectedSuccesses != successes.Count)
+            {
+                throw new ArgumentException("Config file did not contain");
+            }
+        }
+
+        public string[] GetFileContents()
+        {
+            string[] contents = new string[ParameterDictionary.Count + 1];
+            contents[0] = "# WARNING: Changing the values in this file could cause unexpected behaviour if they are invalid! These values are not validated until used.";
+            int lineCount = 1;
+
+            foreach (IConfigParameter p in ParameterDictionary.Values)
+            {
+                contents[lineCount++] = p.GetFileRepresentation();
+            }
+
+            return contents;
+        }
+    }
+}
--- TheBlunatic's Lore Printer 9000/Program.cs
+++ TheBlunatic's Lore Printer 9000/Program.cs
@@ -6,15 +6,80 @@ using System.Drawing;
 using System.Threading.Tasks;
 using System.IO;
 using TextCopy;
+using System.Diagnostics;
 
 namespace LoreApp
 {
+    public struct SplitPath
+    {
+        public string MasterDirectory { get; set; }
+        public string SubDirectory { get; set; }
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+        public SplitPath(string md, string sd, string fn, string fe)
+        {
+            MasterDirectory = md;
+            SubDirectory = sd;
+            FileName = fn;
+            FileExtension = fe;
+        }
+
+        public SplitPath SetMasterDirectory(string to)
+        {
+            return new SplitPath(to, SubDirectory, FileName, FileExtension);
+        }
+        public SplitPath SetSubDirectory(string to)
+        {
+            return new SplitPath(MasterDirectory, to, FileName, FileExtension);
+        }
+        public SplitPath SetFileName(string to)
+        {
+            return new SplitPath(MasterDirectory, SubDirectory, to, FileExtension);
+        }
+        public SplitPath SetFileExtension(string to)
+        {
+            return new SplitPath(MasterDirectory, SubDirectory, FileName, to);
+        }
+        public SplitPath RemoveMasterDirectory()
+        {
+            return new SplitPath(string.Empty, SubDirectory, FileName, FileExtension);
+        }
+        public SplitPath RemoveSubDirectory()
+        {
+            return new SplitPath(MasterDirectory, string.Empty, FileName, FileExtension);
+        }
+        public SplitPath RemoveFileName()
+        {
+            return new SplitPath(MasterDirectory, SubDirectory, string.Empty, FileExtension);
+        }
+        public SplitPath RemoveFileExtension()
+        {
+            return new SplitPath(MasterDirectory, SubDirectory, FileName, string.Empty);
+        }
+
+        public SplitPath ShiftFileDirectoriesToSubDirectory()
+        {
+            int index = FileName.LastIndexOf('\\');
+            if (index == -1) return new SplitPath(MasterDirectory, SubDirectory, FileName, FileExtension);
+            string newSubDirectory = $"{SubDirectory}{FileName.Substring(0, index + 1)}";
+            string newFileName = FileName.Substring(index + 1);
+            return new SplitPath(MasterDirectory, newSubDirectory, newFileName, FileExtension);
+        }
+
+        public static implicit operator string(SplitPath splitPath) => splitPath.ToString();
+        public override string ToString()
+        {
+            return $"{MasterDirectory}{SubDirectory}{FileName}{FileExtension}";
+        }
+    }
     internal class Program
     {
-        const string INPUT_FOLDER = "put images here";
-        const string OUTPUT_FOLDER = "output will be sent here";
+        const string CONFIG_FOLDER = "config";
+        const string CONFIG_PATH = CONFIG_FOLDER + "\\config.txt";
 
         const string LINE_PREFIX = "/customize lore add <i:false>";
+
+        static string ACTIVE_BASE_DIRECTORY;
 
         static Dictionary<string, (Action, string)> COMMANDS = new Dictionary<string, (Action, string)>
         {
@@ -24,11 +89,44 @@ namespace LoreApp
             { "batch", (DoBatch, "Runs convert on every file in the input directory") },
             { "copy", (DoCopy, "Sequentially copy commands to the clipboard") },
             { "clear", (null, "Clear the console") },
+            { "reload", (DoConfigReload, "Reloads config files") },
+            { "config", (DoDisplayConfig, "Displays loaded configuration") },
+            { "whereami", (DoShowActiveBaseDirectory, "Displays the base directory this application is currently active in") },
         };
 
-        static string RemovePrefix(string path) => path.Substring(path.IndexOf('\\'));
-        static string RemovePostfix(string path) => path.Substring(0, path.LastIndexOf('.'));
-        static string GetDirectory(string path) => path.Substring(0, path.LastIndexOf('\\'));
+        static Config CONFIG;
+
+        static SplitPath GetInputFolder()
+        {
+            return new SplitPath(ACTIVE_BASE_DIRECTORY, $"{CONFIG.InputFolder}\\", "", "");
+        }
+        static SplitPath GetOutputFolder()
+        {
+            return new SplitPath(ACTIVE_BASE_DIRECTORY, $"{CONFIG.OutputFolder}\\", "", "");
+        }
+        static SplitPath GetConfigFolder()
+        {
+            return new SplitPath(ACTIVE_BASE_DIRECTORY, $"{CONFIG_FOLDER}\\", "", "");
+        }
+        static SplitPath GetConfigFile()
+        {
+            return new SplitPath(ACTIVE_BASE_DIRECTORY, $"{CONFIG_FOLDER}\\", "config", ".txt");
+        }
+        static SplitPath ConstructSplitPathFromString(string path)
+        {
+            string md = ACTIVE_BASE_DIRECTORY;
+            string rest = path.Substring(ACTIVE_BASE_DIRECTORY.Length);
+            int lastIndexOf = rest.LastIndexOf('\\');
+            string sd = string.Empty;
+            if (lastIndexOf != -1)
+            {
+                sd = rest.Substring(0, lastIndexOf + 1);
+                rest = rest.Substring(lastIndexOf + 1);
+            }
+            lastIndexOf = rest.LastIndexOf('.');
+            return new SplitPath(md, sd, rest.Substring(0, lastIndexOf), rest.Substring(lastIndexOf));
+        }
+
         static string GetInput()
         {
             ConsoleColor old = Console.ForegroundColor;
@@ -91,15 +189,17 @@ namespace LoreApp
             Console.WriteLine(line);
             Console.ForegroundColor = old;
         }
-        static void ConvertFile(string inputPath, string outputPath)
+        static SplitPath ConvertFile(SplitPath inputPath)
         {
+            SplitPath outputPath = inputPath.SetSubDirectory(GetOutputFolder().SubDirectory).SetFileExtension(".txt");
             string[] commands;
             using (Bitmap bmp = new Bitmap(inputPath))
             {
                 commands = GetCommandsFor(bmp);
             }
-            Directory.CreateDirectory(GetDirectory(outputPath));
+            Directory.CreateDirectory(outputPath.ShiftFileDirectoriesToSubDirectory().RemoveFileName().RemoveFileExtension());
             File.WriteAllLines(outputPath, commands);
+            return outputPath;
         }
         static (int, int) ConvertAllInDirectory(string directoryPath)
         {
@@ -111,13 +211,13 @@ namespace LoreApp
                 count.Item2++;
                 try
                 {
-                    ConvertFile(file, $"{OUTPUT_FOLDER}{RemovePrefix(RemovePostfix(file))}.txt");
-                    WriteLine($"Converted {file}", ConsoleColor.Gray);
+                    ConvertFile(ConstructSplitPathFromString(file));
+                    WriteLine($"Converted '{file}'", ConsoleColor.Gray);
                     count.Item1++;
                 }
                 catch (Exception e)
                 {
-                    WriteLine($"Failed to convert {file} ({e.GetType().FullName})", ConsoleColor.Red);
+                    WriteLine($"Failed to convert '{file}' ({e.GetType().FullName})", ConsoleColor.Red);
                 }
             }
             foreach (string directory in directories)
@@ -154,6 +254,30 @@ namespace LoreApp
             }
         }
 
+        static void DoShowActiveBaseDirectory()
+        {
+            Console.WriteLine($"Currently active in '{ACTIVE_BASE_DIRECTORY}'");
+        }
+        static void DoConfigReload()
+        {
+            ReloadConfig();
+            Console.WriteLine("Config reloaded!");
+        }
+        static void DoDisplayConfig()
+        {
+            Console.WriteLine("CONFIGURATION:");
+            foreach (IConfigParameter configParam in CONFIG.ParameterDictionary.Values)
+            {
+                ConsoleColor originColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write($"{configParam.ID}");
+                Console.ForegroundColor = originColor;
+                Console.Write(" = ");
+                Console.ForegroundColor = configParam.GetMostSuitableValueColor();
+                Console.WriteLine(configParam.ToString());
+                Console.ForegroundColor = originColor;
+            }
+        }
         static void DoHelp()
         {
             Console.WriteLine($"COMMANDS:");
@@ -164,16 +288,16 @@ namespace LoreApp
         }
         static void DoConvert()
         {
-            Console.WriteLine("What image would you like to convert? (include the file extension)");
+            Console.WriteLine("What image would you like to convert? (DON'T include the file extension)");
 
             string fileName = GetInput();
 
-            string fileOutput = $"{OUTPUT_FOLDER}\\{RemovePostfix(fileName)}.txt";
-            string fileInput = $"{INPUT_FOLDER}\\{fileName}";
+            SplitPath fileInput = GetInputFolder().SetFileName(fileName).SetFileExtension(".png");
+            SplitPath outputPath;
 
             try
             {
-                ConvertFile(fileInput, fileOutput);
+                outputPath = ConvertFile(fileInput);
             }
             catch (FileNotFoundException)
             {
@@ -186,13 +310,13 @@ namespace LoreApp
                 return;
             }
 
-            Console.WriteLine($"Commands have been added to {fileOutput}!");
+            Console.WriteLine($"Commands have been added to '{outputPath}'!");
         }
         static void DoCopy()
         {
-            Console.WriteLine("What is the name of the output file you would like to copy? (include .txt)");
+            Console.WriteLine("What is the name of the output file you would like to copy? (DON'T include the file extension)");
 
-            string path = $"{OUTPUT_FOLDER}\\{GetInput()}";
+            SplitPath path = GetOutputFolder().SetFileName(GetInput()).SetFileExtension(".txt");
 
             string[] lines;
             try
@@ -201,13 +325,13 @@ namespace LoreApp
             }
             catch (FileNotFoundException)
             {
-                if (path.EndsWith(".txt"))
+                if (path.FileName.EndsWith(".txt"))
                 {
-                    WriteLine("File not found.", ConsoleColor.DarkRed);
+                    WriteLine("File not found. This version of loreprinter requires that you DON'T put a file extension, this may be your issue.", ConsoleColor.DarkRed);
                 }
                 else
                 {
-                    WriteLine("File not found. Did you end with the correct file extension (.txt)?", ConsoleColor.DarkRed);
+                    WriteLine("File not found.", ConsoleColor.DarkRed);
                 }
                 return;
             }
@@ -245,33 +369,90 @@ namespace LoreApp
         }
         static void DoBatch()
         {
-            (int, int) counts = ConvertAllInDirectory(INPUT_FOLDER);
+            (int, int) counts = ConvertAllInDirectory(GetInputFolder());
             Console.WriteLine($"Converted {counts.Item1}/{counts.Item2} encountered files.");
         }
 
+        static void SaveConfig(Config config)
+        {
+            File.Delete(GetConfigFile());
+            File.WriteAllLines(GetConfigFile(), config.GetFileContents());
+        }
+        static void InvalidateConfig()
+        {
+            void ensureDirectory(string directory)
+            {
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                    WriteLine($"WARNING: Missing directory was created: '{directory}'", ConsoleColor.Cyan);
+                }
+            }
+
+            ensureDirectory(GetInputFolder());
+            ensureDirectory(GetOutputFolder());
+        }
+        static void ReloadConfig()
+        {
+            Config returner;
+
+            Directory.CreateDirectory(GetConfigFolder());
+
+            try
+            {
+                string[] configFile = File.ReadAllLines(GetConfigFile());
+                try
+                {
+                    returner = new Config(configFile);
+                }
+                catch
+                {
+                    File.WriteAllLines(GetConfigFile().SetFileName("old_config"), configFile);
+                    returner = new Config();
+                    SaveConfig(returner);
+                    WriteLine($"WARNING: Configuration file was invalid. It has been copied to '{GetConfigFile().SetFileName("old_config")}' and a new default config file has been created: '{GetConfigFile()}'.", ConsoleColor.Cyan);
+                }
+            }
+            catch
+            {
+                returner = new Config();
+                SaveConfig(returner);
+                WriteLine($"WARNING: Could not find configuration file. A new default config file has been created: '{GetConfigFile()}'.", ConsoleColor.Cyan);
+            }
+
+            CONFIG = returner;
+            InvalidateConfig();
+        }
         static void Main(string[] args)
         {
-            Directory.CreateDirectory(INPUT_FOLDER);
-            Directory.CreateDirectory(OUTPUT_FOLDER);
+            ACTIVE_BASE_DIRECTORY = System.Reflection.Assembly.GetEntryAssembly().Location.Substring(0, System.Reflection.Assembly.GetEntryAssembly().Location.LastIndexOf('\\') + 1);
+            
+            ACTIVE_BASE_DIRECTORY = $"{Environment.CurrentDirectory}\\";
 
-            while (true)
+            Console.ForegroundColor = ConsoleColor.Gray;
+            if (!Do(ReloadConfig)) return;
+
+            if (args.Length == 0)
             {
-                WriteLine($"What would you like to do? (type 'help' for a list of commands)", ConsoleColor.Green);
-
-                string choice = GetChoice(COMMANDS.Keys.ToArray());
-
-                if (choice == "clear")
+                while (true)
                 {
-                    Console.Clear();
-                    continue;
-                }
-                else if (choice == "quit" || !Do(COMMANDS[choice].Item1))
-                {
-                    return;
-                }
+                    WriteLine($"What would you like to do? (type 'help' for a list of commands)", ConsoleColor.Green);
 
-                WriteLine("\nPress any key to continue.", ConsoleColor.Yellow);
-                Console.ReadKey(true);
+                    string choice = GetChoice(COMMANDS.Keys.ToArray());
+
+                    if (choice == "clear")
+                    {
+                        Console.Clear();
+                        continue;
+                    }
+                    else if (choice == "quit" || !Do(COMMANDS[choice].Item1))
+                    {
+                        return;
+                    }
+
+                    WriteLine("\nPress any key to continue.", ConsoleColor.Yellow);
+                    Console.ReadKey(true);
+                }
             }
         }
     }
